d1e5a46a06e32c2e85d71f98fc1b047ee9e04ef1
mvn23 <schopdiedwaas@gmail.com>
2025-10-12 12:09:25 +0200

    Update xbacklight keyboard backlight plugin

--- backlight_control/plugins/keyboard_backlight/xbacklight.py
+++ backlight_control/plugins/keyboard_backlight/xbacklight.py
@@ -25,33 +25,41 @@ def get_plugin(hub: LightControlHub, config: dict):
 
 class XBacklightKeyboardBacklight(KeyboardBacklight):
     def __init__(self, hub: LightControlHub, config: dict) -> None:
-        self.config = config
-        self.maximum: int = 100
+        self._config = config
+        self._maximum: int = 100
         self.stored: int = 1
         self._hub: LightControlHub = hub
+
+    @property
+    def config(self) -> dict:
+        return self._config
 
     async def get_current(self) -> int:
         proc = await asyncio.create_subprocess_exec(
             "xbacklight",
             "-ctrl",
-            self.config[CONF_CONTROL],
+            self._config[CONF_CONTROL],
             "-get",
             stdout=asyncio.subprocess.PIPE,
         )
         output, _ = await proc.communicate()
         return int(output)
 
+    @property
+    def maximum(self) -> int:
+        return self._maximum
+
     async def set_absolute(self, value: int) -> None:
         proc = await asyncio.create_subprocess_exec(
             "xbacklight",
             "-ctrl",
-            self.config[CONF_CONTROL],
+            self._config[CONF_CONTROL],
             "-set",
             str(value),
             "-time",
-            str(self.config[CONF_FADE_TIME]),
+            str(self._config[CONF_FADE_TIME]),
             "-fps",
-            str(self.config[CONF_FADE_FPS]),
+            str(self._config[CONF_FADE_FPS]),
         )
         await proc.wait()
 
